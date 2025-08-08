845bcebbcf5b2f2ee163e6453dd54f7713fbf770
최하영 <haing@choehayeong-ui-MacBookPro.local>
2025-08-08 11:47:54 +0900

    first commit

---
+++ src/main/java/Head01_javaprogrammingstart/HelloWorld.java
@@ -0,0 +1,8 @@
+package Head01_javaprogrammingstart;
+
+public class HelloWorld {
+    public static void main(String[] args) {
+        //main이라는 진입점 -> 다른 코드를 실행할때 main을 사용
+        System.out.println("Hello 하영~");
+    }
+}
