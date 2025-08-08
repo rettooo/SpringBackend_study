14930c0d16a57b538bdaeaadff21bda9de312f86
최하영 <haing@choehayeong-ui-MacBookPro.local>
2025-08-08 16:27:34 +0900

    Head01: ex7

---
+++ src/main/java/Head01_javaprogrammingstart/ex7/TwoArray.java
@@ -0,0 +1,28 @@
+package Head01_javaprogrammingstart.ex7;
+
+public class TwoArray {
+    public static void main(String[] args) {
+        int[][] matrix = {
+                {1, 2, 3},
+                {4, 5, 6},
+                {7, 8, 9},
+                {10, 11, 12}
+        };
+//        for(int i = 0;i<matrix.length;i++){
+//            for (int j = 0; j < matrix[i].length; j++) {
+//                System.out.print(matrix[i][j] + " ");
+//            }
+//            System.out.println();
+//
+//        }
+        for(int[] ints: matrix){
+            for(int num: ints){
+                System.out.print(num + " ");
+            }
+            System.out.println();
+        }
+    }
+
+}
+
+
